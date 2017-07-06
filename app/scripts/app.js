5fa98df8d3ab3e22d2c6fcd75cb6e58b5f5846ad
Unknown <matthew.musser@gmail.com>
2017-07-06 14:14:58 -0500

    Checkpoint 3 WIP injected the module into app's dependecy array

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -15,6 +15,6 @@
     }
 
     angular
-        .module('blocChat', ['ui.router', 'firebase', ])
+        .module('blocChat', ['ui.router', 'firebase', 'ui.bootstrap' ])
         .config(config);
  })();
---
+++ app/scripts/controllers/FormCtrl.js
@@ -0,0 +1,15 @@
+(function() {
+    function FormCtrl() {
+        var openModel = function() {
+            
+        }
+
+        var closeModal = function() {
+
+        }
+    }
+
+    angular
+        .module('blocChat')
+        .controller('FormCtrl', ['firebase', 'angularjs', 'angular-animat', 'angular-touch', 'bootstrap-css', FormCtrl]);
+})();
