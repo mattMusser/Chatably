cad09ab2fbc972576e6843f9d7cb0766c3d9bb7c
mattMusser <matthew.musser@gmail.com>
2017-07-10 17:55:27 -0500

    Checkpoint 3 WIP fixed typo and referenced ModalCtrl in Index

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -15,6 +15,6 @@
     }
 
     angular
-        .module('blocChat', ['ui.router', 'firebase', '$uibModal' ])
+        .module('blocChat', ['ui.router', /*'firebase',*/ '$uibModal' ])
         .config(config);
  })();
