ef5a9002b34086a1c9f9456a948f77d2d5b040f1
Unknown <matthew.musser@gmail.com>
2017-07-18 17:04:01 -0500

    Checkpoint 3 complete

--- app/scripts/controllers/HomeCtrl.js
+++ app/scripts/controllers/HomeCtrl.js
@@ -7,21 +7,10 @@
 
         //Room.addRoom("a room");
 
-
-
         /**
         * @function openModal
         * @desc opens modal
         */
-        /*var openModal = function() {
-            $uibModal.open({
-                animation: true,
-                templateUrl: '/templates/modal.html',
-                controller: 'ModalCtrl.js',
-            });
-            console.log("Im being hit");
-        }*/
-
         this.openModal = function () {
             $uibModal.open({
             animation: true,
--- app/scripts/controllers/ModalCtrl.js
+++ app/scripts/controllers/ModalCtrl.js
@@ -5,12 +5,17 @@
     function ModalCtrl(Room, $uibModalInstance) {
         /**
         * @function closeModal
+        * @desc closes the modal
         */
         this.closeModal = function () {
             console.log("cancel is being hit");
             var modelInstance = $uibModalInstance.close('close');
         };
 
+        /**
+        * @function addRoom
+        * @desc adds a new room to room list
+        */
         this.addRoom = function() {
             Room.addRoom(this.newRoomName);
             this.newRoomName = "";
@@ -20,5 +25,5 @@
 
     angular
         .module('blocChat')
-        .controller('ModalCtrl', ['Room', '$uibModalInstance', ModalCtrl]  /*['firebase', 'angularjs', 'angular-animate', 'angular-touch', 'bootstrap.css','$uibModal', ModalCtrl]*/);
+        .controller('ModalCtrl', ['Room', '$uibModalInstance', ModalCtrl]);
 })();
